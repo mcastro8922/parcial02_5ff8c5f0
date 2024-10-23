2aedcdd2ee024727933e0ef50bf981e4ab65bc4a
mcastro89 <mcastro89@cuc.edu.co>
2024-10-23 08:00:00 -0500

    revistas001

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,8 +1,32 @@
 export const Inicio = () => {
     return(
         <>
-        Soy el inicio <br />
-        Welcome!!!
+       <div id="carouselExampleIndicators" className="carousel slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="/public/dia1.jpg" className="d-block w-35" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="/public/dia2.jpg" className="d-block w-35" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="/public/dia3.jpg" className="d-block w-35" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
         </>
     )
 }
